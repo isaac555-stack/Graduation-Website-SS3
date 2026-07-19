2655e47cd83bb119944d3105da40b63b37b49654
Isaac Opara <oparaisaacugochukwu@gmail.com>
2026-07-19 13:48:57 +0100

    Typescript Projects/Graduation/frontend/vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from "@tailwindcss/vite"; // Since you have tailwindcss v4 in package.json
 import path from "path";
 
 export default defineConfig({
+  // FIX: Forces absolute asset paths across deep router refreshes
+  base: "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
